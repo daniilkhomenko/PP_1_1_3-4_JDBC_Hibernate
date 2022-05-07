63428455e2d508cccad8bdecf7a59345bcf1f4a9
daniilkhomenko <daniil.khomenko@yahoo.com>
2022-05-07 10:51:44 +0300

    JDBC homework

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Anna", "TheGreat", (byte) 20);
+        userService.saveUser("Boris", "Johnson", (byte) 25);
+        userService.saveUser("Jason", "Statham", (byte) 31);
+        userService.saveUser("Sasha", "Volkov", (byte) 38);
+        userService.saveUser("Vladimir", "Pozner", (byte) 38);
+
+        userService.removeUserById(1);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+        Util.closeConnection();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static Connection conn;
+
+    public static void connect () {
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/kata_homework1", "root", "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        return conn;
+    }
+
+    //закрывает общий Connection в Main, чтобы переиспользовать его в DAO, а не открывать и закрывать каждый раз заново
+    public static void closeConnection () {
+        try {
+           conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
